940db35417f33277f77db71b3e03cfc3fa2ec086
soulteary <soulteary@gmail.com>
2023-07-24 16:11:56 +0800

    feat: docker image with GPT Code UI release tag

--- scripts/build-gpt-code-ui.sh
+++ scripts/build-gpt-code-ui.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 docker build -t soulteary/gpt-code-ui -f gpt-code-ui/Dockerfile.main . 
+
+VERSION=$(cat gpt-code-ui/VERSION)
+docker build -t "soulteary/gpt-code-ui:${VERSION}" --build-arg="VERSION=${VERSION}" -f gpt-code-ui/Dockerfile.tag . 
